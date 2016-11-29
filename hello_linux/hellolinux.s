85eaecaf57f3dbd6a41528e2da33b96d7a6b1fcb
Ivan Slaev <islaev@lin-0225.ent.mm-sol.com>
2016-11-29 14:04:51 +0200

    hello edit

--- hello_linux/hellolinux.s
+++ hello_linux/hellolinux.s
@@ -4,11 +4,11 @@ _start:
 	movl $4,%eax 	
 	movl $1,%ebx
 	movl $string,%ecx
-	movl $12,%edx
+	movl $13,%edx
 	int $0x80
 	movl $1,%eax
 	movl $0,%ebx
 	int $0x80 
 
 .data
-string: .ascii "Hello, world"
+string: .ascii "Hello, world\n"
